ceb82ce53157e3b577b9d71692e9865cc3f12c77
Amiel Martin <amiel@actionsprout.com>
2017-05-05 10:32:13 -0800

    Add comments
    This commit contains comments for three posts, showcasing the following
    situations:
    
    1. Just data
    
       * Data with just an id (loaded via findRecord)
       * Sideloaded data
       * An attempt to embed data (this also gets loaded via findRecord)
    
    2. Just a link
    
    3. A link and data
    
       * This ends up loading via findRecord (the id wins out over the link)

---
+++ app/adapters/comment.js
@@ -0,0 +1,25 @@
+import DS from 'ember-data';
+
+export const COMMENT_MESSAGES = {
+  '11': "This is comment 11",
+  '12': "This is comment 12",
+  '13': "This is comment 13",
+  '21': "This is comment 21",
+  '22': "This is comment 22",
+  '31': "This is comment 31",
+};
+
+export default DS.JSONAPIAdapter.extend({
+  // For each comment in relationships without data, findRecord is called.
+  findRecord(store, type, id, snapshot) {
+    return {
+      data: {
+        id: id,
+        type: type.modelName,
+        attributes: {
+          message: `${COMMENT_MESSAGES[id]} loaded via findRecord`,
+        },
+      },
+    };
+  },
+});
--- app/adapters/post.js
+++ app/adapters/post.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 
+import { COMMENT_MESSAGES } from './comment';
+
 const {
   RSVP: { resolve },
 } = Ember;
@@ -15,7 +17,22 @@ export default DS.JSONAPIAdapter.extend({
           type: 'post',
           attributes: {
             title: 'This is post #1',
-            body: 'It has content',
+            body: 'This post has only data, but attempts to sideload or embed some records',
+          },
+          relationships: {
+            comments: {
+              data: [
+                { id: 11, type: 'comment' },
+                { id: 12, type: 'comment' },
+                {
+                  id: 13,
+                  type: 'comment',
+                  attributes: {
+                    message: "This comment is embedded (and does not work)",
+                  },
+                },
+              ],
+            },
           },
         },
 
@@ -26,9 +43,77 @@ export default DS.JSONAPIAdapter.extend({
             title: 'This is post #2',
             body: 'It was the best',
           },
+          relationships: {
+            comments: {
+              links: {
+                related: { href: '/posts/2/comments' },
+              },
+            },
+          },
+        },
+
+        {
+          id: 3,
+          type: 'post',
+          attributes: {
+            title: 'This is post #3',
+            body: 'This post has mixed links and data',
+          },
+          relationships: {
+            comments: {
+              links: {
+                related: { href: '/posts/3/comments' },
+              },
+              data: [
+                { id: 31, type: 'comment' },
+              ],
+            },
+          },
         },
 
       ],
+
+      included: [
+        {
+          id: 12,
+          type: 'comment',
+          attributes: {
+            message: 'This comment is side-loaded',
+          },
+        },
+      ],
     });
+  },
+
+  // For each comments relationship with a link (each post, not each comment),
+  // findHasMany is called with that link.
+  findHasMany(store, snapshot, link/*, relationship */) {
+    if (link === '/posts/2/comments') {
+      return resolve({
+        data: [
+          {
+            id: 21,
+            type: 'comment',
+            attributes: {
+              message: `${COMMENT_MESSAGES['21']} loaded via findHasMany`,
+            },
+          },
+        ],
+      });
+    } else if (link === '/posts/3/comments') {
+      return resolve({
+        data: [
+          {
+            id: 31,
+            type: 'comment',
+            attributes: {
+              message: `${COMMENT_MESSAGES['31']} loaded via findHasMany`,
+            },
+          },
+        ],
+      });
+    } else {
+      return this._super(...arguments);
+    }
   },
 });
